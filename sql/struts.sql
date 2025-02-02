5b3e2aa7bcfcbeb36cb971ed274c0659ad862e7d
UNOSHE0516 <hiro0516jms@gmail.com>
2025-02-02 10:55:15 +0900

    　login DTO作る

---
+++ sql/struts.sql
@@ -0,0 +1,12 @@
+ set names utf8;
+ set foreign_key_checks = 0;
+ drop database if exists logindb_yamada;
+ create database logindb_unoki;
+ 	use logindb_yamada;
+ create table user(id int,user_namevarchar(255),password varchar(255));
+
+ insert into user values
+ (1,"taro","123"),
+ (2,"jiro","123"),
+ (3,"hanako","123"),
+ (4,"saburo","123");
