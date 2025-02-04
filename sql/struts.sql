e86425d2b1ad4813acf188f9911040f30f253740
UNOSHE0516 <hiro0516jms@gmail.com>
2025-02-04 22:18:00 +0900

    sqlにデータ保存できやんかったけどできた

--- sql/struts.sql
+++ sql/struts.sql
@@ -1,9 +1,9 @@
  set names utf8;
  set foreign_key_checks = 0;
- drop database if exists logindb_yamada;
+ drop database if exists logindb_unoki;
  create database logindb_unoki;
- 	use logindb_yamada;
- create table user(id int,user_namevarchar(255),password varchar(255));
+ 	use logindb_unoki;
+ create table user(id int,user_name varchar(255),password varchar(255));
 
  insert into user values
  (1,"taro","123"),
